faea69ded5d4ed9a652b40652e3beea57e158dee
Yohualkis <Yohualkis@DESKTOP-FKD43PD>
2024-08-16 21:08:53 -0400

    Realice el modelo estudiante

---
+++ PrimerProyecto/PrimerProyecto/Models/Estudiantes.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimerProyecto.Models;
+
+public class Estudiantes
+{
+    [Key]
+    public int EstudianteId { get; set; } // ID en la BD
+
+    public string? nombre { get; set; }
+    public int matricula { get; set; }
+    public string? carrera { get; set; }
+}

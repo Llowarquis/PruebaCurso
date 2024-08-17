c7d870c117c8e8ecf2b1858bbe3df182e5ee16c3
Yohualkis <Yohualkis@DESKTOP-FKD43PD>
2024-08-16 21:08:15 -0400

    Realice el contexto

---
+++ PrimerProyecto/PrimerProyecto/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimerProyecto.Models;
+
+namespace PrimerProyecto.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Estudiantes> Estudiantes { get; set; }
+}
